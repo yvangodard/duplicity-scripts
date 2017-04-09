d0ecabe8a5fcaec0c68efa9ad4c173d949d4a57d
Yvan GODARD <godardyvan@gmail.com>
2017-04-09 17:27:25 +0200

    correct bug ASCII

--- scripts/master-backup-listing.sh
+++ scripts/master-backup-listing.sh
@@ -8,12 +8,7 @@
 
 export LANG=en_US.utf8
 export LANGUAGE=
-export LC_CTYPE="en_US.utf8"
-export LC_NUMERIC="en_US.utf8"
-export LC_TIME="en_US.utf8"
-export LC_COLLATE="en_US.utf8"
 export LC_MONETARY="en_US.utf8"
-export LC_MESSAGES="en_US.utf8"
 export LC_PAPER=en_US.UTF-8
 export LC_NAME="en_US.utf8"
 export LC_ADDRESS="en_US.utf8"
